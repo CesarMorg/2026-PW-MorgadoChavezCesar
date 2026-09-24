5d4ec78f47dfddfd990e3d5e6b64c8446d40b8f6
CesarMorg <cmorgadoc2400@alumno.ipn.mx>
2026-09-23 23:30:38 -0600

    arreglos

---
+++ PrimerParcial/02Ejercicios/js/app.js
@@ -0,0 +1,45 @@
+
+
+const talleres = [
+  { nombre: 'Introducción a Python', instructor: 'Ing. María López', cupo: 25, inscritos: 25 },
+  { nombre: 'Fundamentos de Redes', instructor: 'Ing. Carlos Ramírez', cupo: 30, inscritos: 18 },
+  { nombre: 'Diseño de Bases de Datos', instructor: 'Ing. Ana Torres', cupo: 20, inscritos: 20 },
+  { nombre: 'Desarrollo Web con JS', instructor: 'Ing. María López', cupo: 25, inscritos: 10 },
+];
+
+
+function pintarTabla(){
+    //debe de obtener la tabla y rellenarla con los datos de talleres
+}
+
+const formArreglos = document.getElementById('form-arreglos');
+const resultadoArreglos = document.getElementById('resultado-arreglo');
+const selectOperacionArreglo = document.getElementById('operacion-arreglo');
+
+formArreglos.addEventListener('submit', (evento) =>{
+    evento.preventDefault();
+    const operacion = selectOperacionArreglo.value;
+
+    let resultado;
+
+    switch(operacion){
+        case 'forEach':
+            resultado = talleres.map((t) => `- ${t.nombre} (${t.inscritos}/${t.cupo})`).join('\n');
+            break;
+    }
+
+    case 'map':
+        resultado = talleres.map((t) => t.nombre).join('\n');
+        break;
+    case 'filter':
+        resultado = talleres
+        .filter((t) => t.inscritos >= t.cupo)
+        .map((t) => t.nombre)
+        .join('\n') || 'No hay talleres llenos'; 
+        break;
+    case 
+    
+
+
+    resultadoArreglos.textContent = resultado;
+});
